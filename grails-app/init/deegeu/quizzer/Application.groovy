0560035fe4695b32cbae27e4e5805042f7b00ef0
Paul Waldo <pwaldo@geico.com>
2017-05-19 12:47:30 -0400

    Create initial project

---
+++ grails-app/init/deegeu/quizzer/Application.groovy
@@ -0,0 +1,10 @@
+package deegeu.quizzer
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
---
+++ grails-app/init/deegeu/quizzer/BootStrap.groovy
@@ -0,0 +1,9 @@
+package deegeu.quizzer
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
